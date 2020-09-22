d200de994a1eaa74981521505660515b4b661104
pashka <59769714+pavelslabikov@users.noreply.github.com>
2020-09-22 18:06:58 +0500

    Создал класс точки входа с методом для output'а

---
+++ main/Main.java
@@ -0,0 +1,22 @@
+package main;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String [] args) {
+        var bot = new ShoppingBot();
+        bot.registerCommands();
+        bot.users.add(new User());
+        var input = new Scanner(System.in);
+        while (true) {
+            for (var user : bot.users) {
+                var userMessage = input.nextLine();
+                bot.receiveMessage(user, userMessage);
+            }
+        }
+    }
+
+    public static void printMessage(String message) {
+        System.out.println(message);
+    }
+}
